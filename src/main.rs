3f006e17bf709cc243891a1aa312d7e8a13080e1
Simon Brand <simon.brand@postadigitale.de>
2022-09-14 00:04:23 +0000

    Use tabs instead of spaces and fix 80 char length

--- src/main.rs
+++ src/main.rs
@@ -16,137 +16,138 @@ use qrcode::QrCode;
 use rand::rngs::OsRng;
 
 pub mod message {
-    include!(concat!(env!("OUT_DIR"), "/qrdata.rs"));
+	include!(concat!(env!("OUT_DIR"), "/qrdata.rs"));
 }
 
 fn create_keypair() -> Keypair {
-    let mut csprng = OsRng{};
-    let keypair: Keypair = Keypair::generate(&mut csprng);
-    return keypair;
+	let mut csprng = OsRng{};
+	let keypair: Keypair = Keypair::generate(&mut csprng);
+	return keypair;
 }
 
 fn sign(keypair: &Keypair, message: &[u8]) -> Signature {
-    let signature = keypair.sign(message);
-    return signature;
+	let signature = keypair.sign(message);
+	return signature;
 }
 
-fn save_keypair_to_file(keypair: Keypair, filename: PathBuf) -> io::Result<()> {
-    let secret_key_bytes = keypair.to_bytes();
-    let mut f = File::create(filename)?;
-    f.write_all(&secret_key_bytes)?;
-    return Ok(());
+fn save_keypair_to_file(key: Keypair, filename: PathBuf) -> io::Result<()> {
+	let secret_key_bytes = key.to_bytes();
+	let mut f = File::create(filename)?;
+	f.write_all(&secret_key_bytes)?;
+	return Ok(());
 }
 
 fn load_keypair_from_file(filename: PathBuf) -> io::Result<Keypair> {
-    let f = File::open(filename)?;
-    let mut reader = BufReader::new(f);
-    let mut buffer = Vec::new();
-    reader.read_to_end(&mut buffer)?;
-    let res = Keypair::from_bytes(&buffer);
-    return match res {
-        Ok(k) => Ok(k),
-        Err(e) => Err(io::Error::new(io::ErrorKind::Other, e)),
-    }
+	let f = File::open(filename)?;
+	let mut reader = BufReader::new(f);
+	let mut buffer = Vec::new();
+	reader.read_to_end(&mut buffer)?;
+	let res = Keypair::from_bytes(&buffer);
+	return match res {
+		Ok(k) => Ok(k),
+		Err(e) => Err(io::Error::new(io::ErrorKind::Other, e)),
+	}
 }
 
 fn show_qrcode(m: &message::QrData) -> Result<(), prost::EncodeError> {
-    let mut buf = Vec::new();
-    buf.reserve(m.encoded_len());
-    m.encode(&mut buf)?;
+	let mut buf = Vec::new();
+	buf.reserve(m.encoded_len());
+	m.encode(&mut buf)?;
 
-    let code = QrCode::new(&buf).unwrap();
-    let string = code.render::<char>()
-        .quiet_zone(false)
-        .module_dimensions(2, 1)
-        .build();
-    println!("{}", string);
-    return Ok(());
+	let code = QrCode::new(&buf).unwrap();
+	let string = code.render::<char>()
+		.quiet_zone(false)
+		.module_dimensions(2, 1)
+		.build();
+	println!("{}", string);
+	return Ok(());
 }
 
 fn sign_qrdata(data: &mut message::QrData, keypair: Keypair) {
-    let to_sign_arr = [
-        data.timestamp.to_be_bytes().to_vec(),
-        data.public_key.clone()];
-    let to_sign: Vec<u8> = to_sign_arr.iter().flat_map(
-        |v| v.iter().copied()).collect();
-    let signature = sign(&keypair, &to_sign);
-    data.signature = signature.to_bytes().to_vec();
+	let to_sign_arr = [
+		data.timestamp.to_be_bytes().to_vec(),
+		data.public_key.clone()];
+	let to_sign: Vec<u8> = to_sign_arr.iter().flat_map(
+		|v| v.iter().copied()).collect();
+	let signature = sign(&keypair, &to_sign);
+	data.signature = signature.to_bytes().to_vec();
 }
 
 enum Action {
-    CreateKey(PathBuf),
-    SignWithKey(PathBuf),
+	CreateKey(PathBuf),
+	SignWithKey(PathBuf),
 }
 
 fn parse_args(args: &Vec<String>) -> Result<Action, String> {
-    if args.len() == 3 {
-        let key_path = Path::new(&args[2]).to_path_buf();
-        let action = &args[1];
-        if action == "create_key" {
-            return Ok(Action::CreateKey(key_path));
-        } else if action == "sign" {
-            return Ok(Action::SignWithKey(key_path));
-        }
-    }
-    return Err("Usage: create_key|sign PATH_TO_KEY".to_string());
+	if args.len() == 3 {
+		let key_path = Path::new(&args[2]).to_path_buf();
+		let action = &args[1];
+		if action == "create_key" {
+			return Ok(Action::CreateKey(key_path));
+		} else if action == "sign" {
+			return Ok(Action::SignWithKey(key_path));
+		}
+	}
+	return Err("Usage: create_key|sign PATH_TO_KEY".to_string());
 }
 
 fn timestamp_now() -> u64 {
-    return match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(k) => k.as_secs(),
-        Err(_) => {
-            panic!("SystemTime before UNIX EPOCH!");
-        },
-    };
+	return match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+		Ok(k) => k.as_secs(),
+		Err(_) => {
+			panic!("SystemTime before UNIX EPOCH!");
+		},
+	};
 }
 
 fn parse_args_and_execute(args: &Vec<String>) -> i32 {
-    let action = match parse_args(&args) {
-        Ok(k) => k,
-        Err(t) => {
-            println!("{}", t);
-            return 1;
-        },
-    };
-    match action {
-        Action::CreateKey(path) => {
-            let keypair = create_keypair();
-            return match save_keypair_to_file(keypair, path) {
-                Ok(()) => 0,
-                Err(e) => {
-                    println!("Error saving keypair: {}", e);
-                    2
-                },
-            };
-        },
-        Action::SignWithKey(path) => {
-            let keypair = match load_keypair_from_file(path) {
-                Ok(k) => k,
-                Err(e) => {
-                    println!("Error loading keypair: {}", e);
-                    return 2;
-                },
-            };
-            let mut msg = message::QrData {
-                public_key: keypair.public.to_bytes().to_vec(),
-                timestamp: timestamp_now(),
-                signature: Vec::new(),
-                personal_information: Vec::new(),
-            };
-            sign_qrdata(&mut msg, keypair);
-            return match show_qrcode(&msg) {
-                Ok(_) => 0,
-                Err(e) => {
-                    println!("Error while encoding qrdata: {}", e);
-                    3
-                },
-            };
-        },
-    };
+	let action = match parse_args(&args) {
+		Ok(k) => k,
+		Err(t) => {
+			println!("{}", t);
+			return 1;
+		},
+	};
+	match action {
+		Action::CreateKey(path) => {
+			let keypair = create_keypair();
+			return match save_keypair_to_file(keypair, path) {
+				Ok(()) => 0,
+				Err(e) => {
+					println!("Error saving key: {}", e);
+					2
+				},
+			};
+		},
+		Action::SignWithKey(path) => {
+			let keypair = match load_keypair_from_file(path) {
+				Ok(k) => k,
+				Err(e) => {
+					println!("Error loading key: {}", e);
+					return 2;
+				},
+			};
+			let mut msg = message::QrData {
+				public_key: keypair.public.to_bytes().to_vec(),
+				timestamp: timestamp_now(),
+				signature: Vec::new(),
+				personal_information: Vec::new(),
+			};
+			sign_qrdata(&mut msg, keypair);
+			return match show_qrcode(&msg) {
+				Ok(_) => 0,
+				Err(e) => {
+					println!("Error while encoding \
+					          qrdata: {}", e);
+					3
+				},
+			};
+		},
+	};
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let result = parse_args_and_execute(&args);
-    std::process::exit(result);
+	let args: Vec<String> = env::args().collect();
+	let result = parse_args_and_execute(&args);
+	std::process::exit(result);
 }
