050cfd006f19ff370883849cf198eaf09a22ead6
Simon Brand <simon.brand@postadigitale.de>
2022-12-02 00:01:09 +0000

    Use protocol from submodule

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/qrdata.proto"], &["src/"])?;
-    return Ok(());
+	prost_build::compile_protos(
+		&["src/cryptographic-id-protocol/cryptographic_id.proto"],
+		&["src/cryptographic-id-protocol/"])?;
+	return Ok(());
 }
