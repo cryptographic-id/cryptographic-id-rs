2e4584313a864a68bb251a25ac365f4737fc66f9
Simon Brand <simon.brand@postadigitale.de>
2023-06-21 00:37:45 +0000

    time.rs: add tests

--- src/time.rs
+++ src/time.rs
@@ -8,3 +8,21 @@ pub fn now() -> u64 {
 		},
 	};
 }
+
+#[cfg(test)]
+mod tests {
+	use std::{thread, time::Duration};
+
+	#[test]
+	fn now() {
+		let now1 = super::now();
+		let now2 = super::now();
+		let now3 = super::now();
+		// Either the second flip was before or after now2, not both
+		assert!(now1 == now2 || now2 == now3);
+		assert!(now1 <= now2 && now2 <= now3);
+		assert!(1686503287 < now1);
+		thread::sleep(Duration::from_secs(1));
+		assert!(now3 < super::now());
+	}
+}
