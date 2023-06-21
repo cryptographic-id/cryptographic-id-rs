75be9dd01a0b9821ebdd3af4ef7ae19efe6d8a8f
Simon Brand <simon.brand@postadigitale.de>
2023-06-21 00:34:28 +0000

    conv.rs: add tests

--- src/conv.rs
+++ src/conv.rs
@@ -8,3 +8,23 @@ pub fn flatten_binary_vec(vec: &Vec<Vec<u8>>) -> Vec<u8> {
 	return vec.iter().flat_map(
 		|v| v.iter().copied()).collect();
 }
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn bytes_to_hex() {
+		let res = super::bytes_to_hex(
+			vec![2, 6, 19, 78, 46, 7, 41, 221, 11]);
+		assert_eq!(res, "02:06:13:4E:2E:07:29:DD:0B");
+	}
+
+	#[test]
+	fn flatten_binary_vec() {
+		let flat = super::flatten_binary_vec(&vec![
+			vec![2, 6, 19],
+			vec![78, 46, 7, 41],
+			vec![],
+			vec![221, 11]]);
+		assert_eq!(flat, vec![2, 6, 19, 78, 46, 7, 41, 221, 11]);
+	}
+}
