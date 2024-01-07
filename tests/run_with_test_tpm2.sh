363e02739bf5ade5913dd3300ca9b49000b4871f
Simon Brand <simon.brand@postadigitale.de>
2024-01-07 01:07:10 +0000

    tests: start swtpm as daemon

--- tests/run_with_test_tpm2.sh
+++ tests/run_with_test_tpm2.sh
@@ -4,6 +4,7 @@ export TCTI="tabrmd:bus_type=session"
 export TPM2TOOLS_TCTI="${TCTI}"
 DIR="$(pwd)/tests/swtpm"
 TMPDIR="$(mktemp -d)"
+SWTPMPIDFILE="${TMPDIR}/swtpm.pid"
 KILL_PIDS=()
 cleanup_pids() {
 	for pid in "${KILL_PIDS[@]}"; do
@@ -29,11 +30,13 @@ swtpm socket \
 	--tpmstate dir="${TMPDIR}/swtpm" \
 	--flags startup-clear \
 	--ctrl type=tcp,port=2322 \
-	--server type=tcp,port=2321 &
-KILL_PIDS+=("$!")
-sleep 0.1 # Wait until started
+	--server type=tcp,port=2321 \
+	--daemon \
+	--pid file="${SWTPMPIDFILE}"
+KILL_PIDS+=("$(cat "${SWTPMPIDFILE}")")
 # in gitlab container, tests are running as root
 /usr/sbin/tpm2-abrmd --session --allow-root --tcti=swtpm &
+sleep 0.1 # Wait until started
 KILL_PIDS+=("$!")
 sha1="0000000000000000000000000000000000000000"
 sha256="0000000000000000000000000000000000000000000000000000000000000000"
